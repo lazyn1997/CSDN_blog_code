e6e077b495870267db9f41fc462bc0a4bdb45cb7
lazyn1997 <34644177+lazyn1997@users.noreply.github.com>
2023-03-24 18:56:04 +0800

    Update AStar.py
    Synchronize blog changes

--- 课后作业/机器学习/A*算法迷宫寻路/AStar.py
+++ 课后作业/机器学习/A*算法迷宫寻路/AStar.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on Sat Nov 24 16:47:38 2018
 
@@ -19,25 +20,19 @@ def gs(i, j):
     return abs(i - startx) + abs(j - starty)
 
 
-def op(Open, i, j, minf):
-    global i1
-    global j1
-    f = gs(i, j) + h2(i, j)
-    if [i + 1, j + 1] not in Close:
-        Open.append([i + 1, j + 1])
-    if f < minf:
-        minf = f
-        i1 = i
-        j1 = j
-    return Open, minf
-
-
 # a = np.mat(np.random.randint(0, 2, size=[10, 10]))
 a = np.mat([[0, 0, 0, 0, 0],
             [1, 0, 1, 0, 1],
-            [0, 0, 1, 1, 1],
-            [0, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 0],
             [0, 0, 0, 1, 0]])
+
+# a = np.mat([[0, 0, 0, 0, 0],
+#             [1, 0, 1, 0, 1],
+#             [0, 0, 1, 1, 1],
+#             [0, 1, 0, 0, 0],
+#             [0, 0, 0, 1, 0]])
+
 # a = np.mat([[0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1],
 #             [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1],
 #             [0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
@@ -67,52 +62,73 @@ for l in range(len(a)):
 print('')
 
 startx, starty = 1, 1
-i, j = startx - 1, starty - 1
 endx, endy = 4, 4
-Open, Close = [[startx, starty]], [[startx, starty]]
-Opens = [[startx, starty]]
-road = 100
-start = time.time()
+if a[startx - 1, starty - 1] == 1:
+    print("起点%s位置为墙，最短路径寻找失败！" % ([startx, starty]))
+else:
+    # 存储已扩展结点，即所有寻找的节点
+    Close = [[startx, starty]]
+    # 存储已生成节点，即最终走过的节点
+    Opens = [[startx, starty]]
+    # 存储未走的分叉节点
+    crossings = []
+    # 设置在原地图行走的路径值
+    road = 100
+    start = time.time()
+    rows, cols = a.shape
+    while True:
+        # 判断最后走过的节点是否为终点
+        if Close[-1] != [endx, endy]:
+            Open = []
+            # 减1得到数组下标值
+            i, j = Close[-1][0] - 1, Close[-1][1] - 1
+            # 对当前节点上下左右四个节点进行判断
+            for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if [ni + 1, nj + 1] not in Opens and 0 <= ni < rows and 0 <= nj < cols and a[ni, nj] == 0:
+                    Open.append([ni + 1, nj + 1])
+            # 将已走过的节点值修改为路径值，并将路径值加1
+            a[i, j] = road
+            road = road + 1
+            if Open:
+                # 对所有扩展到的节点进行排序，reverse=True 结果从大到小，即尾部存储代价最小的节点
+                Open = sorted(Open, key=lambda x: gs(x[0], x[1]) + h2(x[0], x[1]), reverse=True)
+                Opens.extend(Open)
+                # 将代价最小的节点存储到 Close 表
+                Close.append(Open.pop())
+                # 如果 pop 后 Open 表不为空，说明存在岔路，将岔路存储到 crossings 中
+                if Open:
+                    crossings.extend(Open)
+            # 判断是否存在未走过的分叉节点
+            elif crossings:
+                next_way = crossings.pop()
+                # 实现路径回退，循环条件为回退节点与分叉节点不相邻
+                while sum((np.array(Close[-1]) - np.array(next_way)) ** 2) != 1:
+                    # 当一条路径寻找失败后，是否将该路径岔路后的部分恢复为原地图
+                    # i, j = Close[-1]
+                    # a[i-1, j-1] = 0
+                    # 每次 while 循环路径值 road 减1，并弹出 Close 表中的节点
+                    road -= 1
+                    Close.pop()
+                # 将 crossings 中最后一个节点添加到 Close 表
+                Close.append(next_way)
+            else:
+                print("最短路径寻找失败，失败位置为：%s，路径为：" % Close[-1])
+                break
+        else:
+            a[endx - 1, endy - 1] = road
+            print("最短路径寻找成功，路径为：")
+            break
+    for r in range(rows):
+        for c in range(cols):
+            # 0表示format中第一个元素，>表示右对齐输出，4表示占四个字符
+            print("{0: >4}".format(a[r, c]), end='')
+        print('')
+    end = time.time()
+    print("\n扩展节点数为：%d, 生成节点数为：%d, 用时为 %.8f" % (len(Opens), len(Close), float(end - start)))
 
-while Open:
-    if a[startx - 1, starty - 1] == 1:
-        print("最短路径寻找失败，失败位置为：", Close[-1], ", 点的移动轨迹为：")
-        break
-    if Open[-1] != [endx, endy]:
-        Open = []
-        minf = 10000
-        if i + 1 < len(a) and a[i + 1, j] == 0:
-            Open, minf = op(Open, i + 1, j, minf)
-        if i - 1 >= 0 and a[i - 1, j] == 0:
-            Open, minf = op(Open, i - 1, j, minf)
-        if j + 1 < len(a) and a[i, j + 1] == 0:
-            Open, minf = op(Open, i, j + 1, minf)
-        if j - 1 >= 0 and a[i, j - 1] == 0:
-            Open, minf = op(Open, i, j - 1, minf)
-        a[i, j] = road
-        road = road + 1
-        if Open:
-            i, j = i1, j1
-            Close.append([i + 1, j + 1])
-            Opens.extend(Open)
-    else:
-        end = time.time()
-        a[endx - 1, endy - 1] = road
-        print("最短路径寻找成功，路径为：")
-        for l in range(len(a)):
-            for m in range(a[0].size):
-                # 0表示format中第一个元素，>表示右对齐输出，3表示占三个字符
-                print("{0: >4}".format(a[l, m]), end='')
-            print('')
-        print("扩展节点数为：", len(Opens))
-        print("生成节点数为：", len(Close))
-        print('用时为:', end - start)
-        print('Close表为:', Close)
-        print("点的移动轨迹为：")
-        break
-if not Open:
-    print("最短路径寻找失败，失败位置为：", Close[-1], ", 点的移动轨迹为：")
-for k in range(len(Close)):
-    print(Close[k], end='')
-    if k < len(Close) - 1:
-        print("-->", end='')
+    print('Close表为：%s' % Close)
+    print("点的移动轨迹为：")
+    for k in range(len(Close)):
+        print(Close[k], end='')
+        if k < len(Close) - 1:
+            print("-->", end='')
